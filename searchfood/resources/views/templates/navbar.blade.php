39451d4df7f30039ecf84ad1cee635a26ba24fda
Danilo8 <danilodesenvolvedorweb@hotmail.com>
2018-08-28 19:02:08 -0300

    Template Navbar criada

---
+++ searchfood/resources/views/templates/navbar.blade.php
@@ -0,0 +1,18 @@
+<!--CSS do Template-->
+<link rel="stylesheet" href="{{asset('css/navbar.css')}}">
+
+<!--Estrutura do Template-->
+<nav id="template-navbar" class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="{{url('/')}}">
+        <img height="55" src="{{asset('img/logo/logo.png')}}" alt="Logo">
+        <span id="logo-text">Search Food</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            
+        </ul>
+    </div>
+</nav>
