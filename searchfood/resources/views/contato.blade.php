d22b9cd56e4c6248d1613bc9b60fe1a19dd9764b
Danilo8 <danilodesenvolvedorweb@hotmail.com>
2018-08-28 19:20:35 -0300

    Tamplate Navbar Concluida

--- searchfood/resources/views/contato.blade.php
+++ searchfood/resources/views/contato.blade.php
@@ -1,1 +1,20 @@
-Oi
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Search Food</title>
+
+        <!--cdn Bootstrap CSS file-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <!-- Jquery and Bootstrap JS Script files-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        @include('templates.navbar')
+    </body>
+</html>
--- searchfood/resources/views/templates/navbar.blade.php
+++ searchfood/resources/views/templates/navbar.blade.php
@@ -1,6 +1,10 @@
 <!--CSS do Template-->
 <link rel="stylesheet" href="{{asset('css/navbar.css')}}">
 
+<!--Fonts-->
+<link href="https://fonts.googleapis.com/css?family=Copse" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+
 <!--Estrutura do Template-->
 <nav id="template-navbar" class="navbar navbar-expand-lg navbar-light">
     <a class="navbar-brand" href="{{url('/')}}">
@@ -12,7 +16,31 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-            
+            <div class="dropdown nav-item">
+                <span style="cursor: pointer" class="nav-link">
+                    Sobre
+                </span>                        
+                <div class="dropdown-content text-center p-1">
+                    <a class="dropdown-item" href="{{url('/sobre')}}">Sobre o Search Food</a>
+                    <a class="dropdown-item" href="{{url('/termos')}}">Termos de Uso</a>
+                    <a class="dropdown-item" href="{{url('/privacidade')}}">Privacidade</a>
+                </div>
+            </div>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="{{url('/contato')}}">Contato</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{url('/entrar')}}">Entrar/Cadastrar</a>
+            </li>
+            <div id="donos-restaurante" class="dropdown nav-item">
+                <span style="cursor: pointer" class="nav-link">
+                    Donos de Restaurante
+                </span>                        
+                <div class="dropdown-content text-center p-1">
+                    <a class="dropdown-item" href="{{url('/restaurante/cadastro')}}">Cadastrar Restaurante</a>
+                    <a class="dropdown-item" href="{{url('/restaurante/login')}}">Acessar Painel</a>
+                </div>
+            </div>
         </ul>
     </div>
 </nav>
